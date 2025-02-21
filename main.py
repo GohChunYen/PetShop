716bb17eec2ce2ad6204604a58d49b5261463220
Chun Yen <chunyen.goh@bertelsmann.de>
2025-02-21 10:25:25 +0800

    Reformat code, rectify code error

--- main.py
+++ main.py
@@ -5,20 +5,23 @@ from db.session import engine
 from routers import owners, pets
 from config import settings
 
-def create_tables():         
-	Base.metadata.create_all(bind=engine)
-        
-        
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+
 def start_application():
-    app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
     create_tables()
     return app
+
 
 app = start_application()
 
 app.include_router(owners.router)
 app.include_router(pets.router)
 
+
 @app.get("/")
 def home():
-    return {"msg":"Welcome to PetShop"}
+    return {"msg": "Welcome to PetShop"}
